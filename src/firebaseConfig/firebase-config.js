e88a4ffea158349657c8d4bcb322b58e45a94823
hanbee95 <nospicynocilantro@gmail.com>
2022-11-27 15:18:49 -0600

    enabling .env file

--- src/firebaseConfig/firebase-config.js
+++ src/firebaseConfig/firebase-config.js
@@ -9,13 +9,13 @@ import {
 import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC5WVi9PIMBNO1B-1O39d0PLGHjbNJIHpc",
-    authDomain: "the-oh--code.firebaseapp.com",
-    projectId: "the-oh--code",
-    storageBucket: "the-oh--code.appspot.com",
-    messagingSenderId: "1065793139893",
-    appId: "1:1065793139893:web:bb1bc1e0e5dd12898c7afc",
-    measurementId: "G-1X9PRFKRFE"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
   };
 const app = initializeApp(firebaseConfig);  
 export const db = getFirestore(app);
