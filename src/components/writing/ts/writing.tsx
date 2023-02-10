38830e127f17ee0b19fe0eefa91983cab9048421
hanbee95 <nospicynocilantro@gmail.com>
2023-02-09 19:54:06 -0600

    added input value txt to DB by using the enroll btn

--- src/components/writing/ts/writing.tsx
+++ src/components/writing/ts/writing.tsx
@@ -1,11 +1,30 @@
-import React , { useState }from 'react';
+import React , { useState, useRef }from 'react';
 import "../scss/writing.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
+import { getFirestore, collection, addDoc  } from "firebase/firestore";
+import { app } from "../../../firebaseConfig/firebase-config";
+
+
 function Writing() {
-
-
+  var getValue =''
+  const [message, setMessage] = useState('');
+  const handleMessageChange = event => {
+    // 👇️ access textarea value
+    getValue = event.target.value;
+    console.log(getValue);
+  };
+  async function putDB(){
+    // const ref = useRef(null);
+    console.log('abcabc')
+    // var txt = ref.current.value;
+    const db = getFirestore(app);
+    const docRef = await addDoc(collection(db, "cities"), {
+      name: "Tokyo",
+      text: getValue
+    });
+  }
   return (
     <div className='writePage'>
       <div className='colBar'>
@@ -16,9 +35,13 @@ function Writing() {
             <div className='titlehead'>
               글쓰기
             </div>
-            <div className='enrollbtn'>등록</div>
+            <div className='enrollbtn' onClick={putDB}>등록</div>
           </div>
           <div className='headbar'></div>
+          <input name="textbody" id='textbody'
+          // value={message}
+          spellCheck='false'
+          onChange={handleMessageChange}/>
         </div>
       하이하이
       </div>
