2e294f3ca4655eafcbc124de58eb9f2893e769b3
hanbee95 <nospicynocilantro@gmail.com>
2023-02-11 20:02:36 -0600

    title/boardtitle in html css

--- src/components/writing/ts/writing.tsx
+++ src/components/writing/ts/writing.tsx
@@ -37,6 +37,19 @@ function Writing() {
             <div className='enrollbtn' onClick={putDB}>등록</div>
           </div>
           <div className='headbar'></div>
+          <div className='board'>
+            <div className='boardtitle'>게시판</div>
+            <select name='게시판 설정' className='boardSetting'>
+              <option value="게시판선택">게시판선택</option>
+              <option value="밴픽게시판">밴픽게시판</option>
+              <option value="자유게시판">자유게시판</option>
+            </select>
+            </div>
+          <div className='title'>
+            <div className='titleUser'>제목</div>
+            <input className='titleWrite' placeholder="제목을 입력해 주세요"></input>
+          </div>
+          <div className='attach'></div>
           <textarea name="textbody" id='textbody' className="textbody"
           // value={message}
           spellCheck='false'
