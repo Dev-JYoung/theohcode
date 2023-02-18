e9312c86608e73fd31777d4470648ba89ff7b847
hanbee95 <nospicynocilantro@gmail.com>
2023-02-18 14:17:12 -0600

    db to boardfree console log

--- src/components/communityBoard/ts/communityBoard.tsx
+++ src/components/communityBoard/ts/communityBoard.tsx
@@ -3,9 +3,22 @@ import "../scss/communityBoard.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-import { getFirestore, collection, addDoc  } from "firebase/firestore";
+import { collection, query, where, getDocs, getFirestore  } from "firebase/firestore";
 import { app } from "../../../firebaseConfig/firebase-config";
 
+async function putDB(){
+  // const ref = useRef(null);
+  console.log('upload to firebase DB')
+  // var txt = ref.current.value;
+  const db = getFirestore(app);
+  // const q = query(collection(db, "cities"), where("capital", "==", true));
+  const q = query(collection(db, "write"));
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+  });
+}
 
 function CommunityBoard() {
 
@@ -25,7 +38,8 @@ function CommunityBoard() {
             <div className='titlehead'>
               밴픽게시판
             </div>            
-          </div>          
+          </div> 
+          <div className='enrollbtn' onClick={putDB}>등록</div>         
         </div>
       </div>
 
