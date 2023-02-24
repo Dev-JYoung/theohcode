2daef1868398958ff8d213414faa978ca57e9830
hanbee95 <nospicynocilantro@gmail.com>
2023-02-23 22:21:05 -0600

    page diff

--- src/components/communityBoard/ts/communityBoard.tsx
+++ src/components/communityBoard/ts/communityBoard.tsx
@@ -22,8 +22,35 @@ import { app } from "../../../firebaseConfig/firebase-config";
 //   });
 // }
 
+function pageAlgo(total, bottomSize, listSize, cursor ){
+  //total = 총 갯수
+  //bottomSize = 하단크기
+  //listSize = 화면에서 보여줄 크기
+  //cursor = 현재 나의 페이지
+
+  let totalPageSize = Math.ceil(total / listSize)  //한 화면에 보여줄 갯수에서 구한 하단 총 갯수 
+
+  let firstBottomNumber = cursor - cursor % bottomSize + 1;  //하단 최초 숫자
+  let lastBottomNumber = cursor - cursor % bottomSize + bottomSize;  //하단 마지막 숫자
+
+  if(lastBottomNumber > totalPageSize) lastBottomNumber = totalPageSize  //총 갯수보다 큰 경우 방지
+
+  return {
+      firstBottomNumber,
+      lastBottomNumber,
+      totalPageSize,
+      total,
+      bottomSize,
+      listSize,
+      cursor
+  }
+}
+
+
 function CommunityBoard() {
+  var numberDB = 0;
   const db = getFirestore(app);
+  
   const [todos, setTodos] = useState([]);
    
   const fetchPost = async () => {
@@ -34,10 +61,31 @@ function CommunityBoard() {
                   .map((doc) => ({...doc.data(), id:doc.id }));
               setTodos(newData);                
               console.log(todos, newData);
+              numberDB = querySnapshot.size;
+              console.log(numberDB)
+              
           })
      
   }
-  
+  console.log('check point 3')
+  console.log(numberDB)
+  var MyDiv2 = document.getElementById('page');
+  // var doc = document.getElementsByClassName("page");
+  let pagenums = '';
+
+  let info = pageAlgo(numberDB, 20, 10, 1);
+  console.log(numberDB);
+  console.log(info.lastBottomNumber);
+  console.log(info.firstBottomNumber);
+  for(let i = info.firstBottomNumber ; i <= info.lastBottomNumber; i++){
+    console.log("check");
+    console.log(i);
+    // i === info.cursor ? console.log(`<span>cur : ${i}</span>`) : console.log(`<span>${i}</span>`);
+    // MyDiv2 = MyDiv2 + '<h3>hello<h3>';
+    MyDiv2.innerHTML = MyDiv2.innerHTML + {i};
+    // pagenums.concat('<span>${i}</span>')
+  }
+  console.log(pagenums)
   useEffect(()=>{
       fetchPost();
   }, []);
@@ -84,6 +132,9 @@ function CommunityBoard() {
             }
 
           </div>   
+        </div>
+        <div className='page' id='page'>
+            {pagenums}
         </div>
       </div>
 
