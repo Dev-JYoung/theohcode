b3d1fe891f748b50482b6af461583ca464d1128b
hanbee95 <nospicynocilantro@gmail.com>
2023-03-02 21:21:59 -0600

    login status show in frontend

--- src/components/header/ts/header.tsx
+++ src/components/header/ts/header.tsx
@@ -2,16 +2,28 @@ import React from "react";
 import '../scss/header.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faLock } from "@fortawesome/free-solid-svg-icons";
-import { signInWithGoogle } from "../../../firebaseConfig/firebase-config";
+import { signInWithGoogle, auth } from "../../../firebaseConfig/firebase-config";
 
 function Header() {
+  var user = auth.currentUser;
+  var loginchange = '';
+  if (user){
+    loginchange = user.displayName;
+  }
+  else{
+    loginchange = '<FontAwesomeIcon className="lockIcon" icon={faLock}/>Log In';
+  }
 
   return(
     <div className="mainPageHeader">
       <div className="mainPageDetail">
         <span className="mainPageLogoText">THEOHCODE</span>
-        <div className="mainPageLogIn" onClick={signInWithGoogle}>
-          <span className="mainPageLoginText"><FontAwesomeIcon className="lockIcon" icon={faLock}/>Log In</span>
+        <div className="mainPageLogIn" onClick={signInWithGoogle}>  
+        {
+          user ? 
+          <span className="mainPageLoginText">{user.displayName}</span>
+          :<span className="mainPageLoginText"> <FontAwesomeIcon className="lockIcon" icon={faLock}/>Log In</span>
+        }
         </div>
       </div>
     </div>
